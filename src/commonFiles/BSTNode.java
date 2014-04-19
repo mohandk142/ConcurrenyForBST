8d26bd432e5f47735c627a6d3aca23f30fbf6eca
sanchitaroy <sanchita@cise.ufl.edu>
2014-04-19 15:01:37 -0400

    updated the code

---
+++ src/commonFiles/BSTNode.java
@@ -0,0 +1,38 @@
+package commonFiles;
+
+public class BSTNode<E> {
+	
+	private E key;
+	private BSTNode<E> left, right;
+	
+	public BSTNode(E key, BSTNode<E> left, BSTNode<E> right){
+		this.key = key;
+		this.left = left;
+		this.right = right;
+	}
+	
+	public E getKey(){
+		return key;
+	}
+	
+	public BSTNode<E> getLeft(){
+		return left;
+	}
+	
+	public BSTNode<E> getRight(){
+		return right;
+	}
+	
+	public void setKey(E newK) {
+		key = newK;
+	}
+	
+	public void setLeft(BSTNode<E> newL){
+		left = newL;
+	}
+	
+	public void setRight(BSTNode<E> newR){
+		right = newR;
+	}
+
+}
---
+++ src/nonConcurrentBST/NCBST.java
@@ -0,0 +1,132 @@
+package nonConcurrentBST;
+
+import commonFiles.BSTNode;
+
+public class NCBST<E extends Comparable<E>> {
+	
+	private BSTNode<E> root;
+	
+	public NCBST(){
+		root = null;
+	}
+	
+	public BSTNode<E> getRoot() {
+		return root;
+	}
+
+	public void setRoot(BSTNode<E> root) {
+		this.root = root;
+	}
+
+	public void insert(E key){
+		root = insert(root, key);
+	}
+	
+	private BSTNode<E> insert(BSTNode<E> n, E key) {
+	    if (n == null) {
+	        return new BSTNode<E>(key, null, null);
+	    }
+	     
+	    if (n.getKey().equals(key)) {
+	    	n.setLeft( insert(n.getLeft(), key) );
+	        return n;
+	        //System.out.println("Duplicate key");
+	        //return null;
+	    }
+	    
+	    if (key.compareTo(n.getKey()) < 0) {
+	        n.setLeft( insert(n.getLeft(), key) );
+	        return n;
+	    }
+	    
+	    else {
+	        n.setRight( insert(n.getRight(), key) );
+	        return n;
+	    }
+	}
+	
+	public void delete(E key){
+		root = delete(root, key);
+	}
+	
+	private BSTNode<E> delete(BSTNode<E> n, E key) {
+	    if (n == null) {
+	        return null;
+	    }
+	    
+	    if (key.equals(n.getKey())) {
+
+	        if (n.getLeft() == null && n.getRight() == null) {
+	            return null;
+	        }
+	        if (n.getLeft() == null) {
+	            return n.getRight();
+	        }
+	        if (n.getRight() == null) {
+	            return n.getLeft();
+	        }
+	       
+	        E smallVal = smallest(n.getRight());
+	        n.setKey(smallVal);
+	        n.setRight( delete(n.getRight(), smallVal) );
+	        return n; 
+	    }
+	    
+	    else if (key.compareTo(n.getKey()) < 0) {
+	        n.setLeft( delete(n.getLeft(), key) );
+	        return n;
+	    }
+	    
+	    else {
+	        n.setRight( delete(n.getRight(), key) );
+	        return n;
+	    }
+	}
+	
+	public boolean lookup(E key){
+		return lookup(root, key);		
+	}
+	
+	private boolean lookup(BSTNode<E> n, E key){
+		if(n == null){
+			return false;
+		}
+		
+		if(n.getKey().equals(key)){
+			return true;
+		}
+		
+		if(key.compareTo(n.getKey()) < 0){
+			return lookup(n.getLeft(), key);
+		}
+		
+		else{
+			return lookup(n.getRight(), key);
+		}
+	}
+	
+	public int[] search(E key){
+		return null;
+		
+	}
+	
+	public void traversal(BSTNode<E> root){
+
+		if(null == root){
+			return;
+		}	
+		traversal(root.getLeft());
+		System.out.println(root.getKey());
+		traversal(root.getRight());
+	}
+	
+	private E smallest(BSTNode<E> n){
+		
+	    if (n.getLeft() == null) {
+	        return n.getKey();
+	    } else {
+	        return smallest(n.getLeft());
+	    }
+	}
+
+}
